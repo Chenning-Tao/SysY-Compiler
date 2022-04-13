188f4ac92e3bc3dc584c65f30bef0530c7237e56
Chenning-Tao <58070700+Chenning-Tao@users.noreply.github.com>
2022-04-13 13:13:27 +0800

    restructure file

--- ast/ast.hpp
+++ ast/ast.hpp
@@ -2,19 +2,33 @@
 // Created by tcn on 22-4-11.
 //
 
-#ifndef COMPILER_AST_H
-#define COMPILER_AST_H
+#ifndef COMPILER_AST_HPP
+#define COMPILER_AST_HPP
 #include <iostream>
 #include <memory>
+#include <map>
+#include <llvm-14/llvm/IR/Value.h>
+#include <llvm-14/llvm/ADT/APFloat.h>
+#include <llvm-14/llvm/ADT/STLExtras.h>
+#include <llvm-14/llvm/IR/BasicBlock.h>
+#include <llvm-14/llvm/IR/Constants.h>
+#include <llvm-14/llvm/IR/DerivedTypes.h>
+#include <llvm-14/llvm/IR/Function.h>
+#include <llvm-14/llvm/IR/IRBuilder.h>
+#include <llvm-14/llvm/IR/LLVMContext.h>
+#include <llvm-14/llvm/IR/Module.h>
+#include <llvm-14/llvm/IR/Type.h>
+#include <llvm-14/llvm/IR/Verifier.h>
 using namespace std;
+
+static void print_indent(int indent) {
+    for(int i = 0; i < indent; ++i) cout << "\t";
+}
 
 // all AST base class
 class BaseAST {
 private:
 public:
-    static void print_indent(int indent) {
-        for(int i = 0; i < indent; ++i) cout << "\t";
-    }
     virtual ~BaseAST() = default;
     virtual void print(int indent) const = 0;
 };
@@ -22,60 +36,40 @@ public:
 class CompUnitAST : public BaseAST {
 public:
     unique_ptr<BaseAST> func_def;
-    void print(int indent) const override{
-        cout << "CompUnit {" << endl;
-        ++indent;
-        print_indent(indent);
-        func_def->print(indent);
-        cout << "}" << endl;
-    }
+
+    void print(int indent) const override;
 };
+
 
 class FuncDefAST : public BaseAST {
 public:
     unique_ptr<BaseAST> func_type;
     string ident;
     unique_ptr<BaseAST> block;
-    void print(int indent) const override{
-        cout << "FuncDef { " << endl;
-        ++indent;
-        print_indent(indent);
-        func_type->print(indent);
-        cout << ", " << ident << ", " ;
-        block->print(indent);
-        print_indent(--indent);
-        cout << " }" << endl;
-    }
+
+    void print(int indent) const override;
 };
 
 class FuncTypeAST : public BaseAST {
 public:
     string type;
-    void print(int indent) const override{
-        cout << "FuncType :" << type;
-    }
+
+    void print(int indent) const override;
 };
 
 class BlockAST : public BaseAST {
 public:
     unique_ptr<BaseAST> block_item;
-    void print(int indent) const override{
-        cout << "Block {" << endl;
-        ++indent;
-        print_indent(indent);
-        block_item->print(indent);
-        print_indent(--indent);
-        cout << "}" << endl;
-    }
+
+    void print(int indent) const override;
 };
 
 class StmtAST : public BaseAST {
 public:
     string re;
     int number;
-    void print(int indent) const override{
-        cout << re << " " << number << endl;
-    }
+
+    void print(int indent) const override;
 };
 
-#endif //COMPILER_AST_H
+#endif //COMPILER_AST_HPP
