a0bad79f08614749181f947a8de9145dcc4c4a06
Your Name <451966708@qq.com>
2022-05-29 16:39:20 +0800

    add function like macro

--- frontend/macro.cpp
+++ frontend/macro.cpp
@@ -4,23 +4,46 @@
 
 #include "macro.hpp"
 
-bool macro::insert(string a, string b) {
-    if (a.length() == 0) {
-        return false;
-    }
-    if (b.length() == 0) {
-        return false;
-    }
-    if (macro_map.find(a) != macro_map.end()) {
-        return false;
-    }
+bool macro::add_macro(const string& a, const string& b) {
+    if (a.length() == 0 || b.length() == 0) return false;
+    if (macro_map.find(a) != macro_map.end()) return false;
     macro_map[a] = b;
     return true;
 }
 
-string macro::get(string a) {
-    if (macro_map.find(a) == macro_map.end()) {
-        return "";
-    }
+bool macro::add_macro(const string& a, const string& b, string var_list){
+    if (a.length() == 0 || b.length() == 0) return false;
+    if (macro_map.find(a) != macro_map.end()) return false;
+    macro_map[a] = b;
+    regex delimiters("[ ]*,[ ]*");
+    vector<string> v(sregex_token_iterator(var_list.begin(),var_list.end(),delimiters,-1),
+                                sregex_token_iterator());
+    macro_var[a] = v;
+    return true;
+}
+
+int macro::function(const string& a) {
+    if (macro_map.find(a) == macro_map.end()) return 0;
+    else if (macro_var[a].empty()) return 1;
+    else return 2;
+}
+
+string macro::get_macro(const string& a) {
     return macro_map[a];
+}
+
+string macro::get_macro(const string& a, string var_list) {
+    regex delimiters("[ ]*,[ ]*");
+    vector<string> v(sregex_token_iterator(var_list.begin(),var_list.end(),delimiters,-1),
+                     sregex_token_iterator());
+    vector<string> var = macro_var[a];
+    string expand = macro_map[a];
+    for(int i = 0; i < var.size(); ++i){
+        size_t pos = expand.find(var[i]);
+        while(pos != string::npos){
+            expand.replace(pos, v[i].size(), v[i]);
+            pos = expand.find(var[i]);
+        }
+    }
+    return expand;
 }
--- frontend/macro.hpp
+++ frontend/macro.hpp
@@ -6,13 +6,20 @@
 #define COMPILER_MACRO_HPP
 #include <unordered_map>
 #include <cstring>
+#include <vector>
+#include <regex>
+#include <iostream>
 
 using namespace std;
 class macro {
     unordered_map<string, string> macro_map;
+    unordered_map<string, vector<string>> macro_var;
 public:
-    bool insert(string a, string b);
-    string get(string a);
+    bool add_macro(const string& a, const string& b);
+    bool add_macro(const string& a, const string& b, string var_list);
+    int function(const string& a);
+    string get_macro(const string& a);
+    string get_macro(const string& a, string var_list);
 };
 
 
